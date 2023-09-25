f9b995677c7b85da4b1df622de878a18c4496b2c
Frank Machaca <58697305+piratax@users.noreply.github.com>
2023-09-25 07:13:20 -0500

    Actualizacion
    Actualizacion

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Exam1"
+include(":app")
+ 
